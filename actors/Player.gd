a65c126d05f0eaf08ddef0bb9d4fd7fd32a171e0
rockgem <63050183+rockgem@users.noreply.github.com>
2024-01-05 16:43:58 +0800

    asdasd

--- actors/Player.gd
+++ actors/Player.gd
@@ -2,10 +2,21 @@ extends CharacterBody2D
 
 var move_speed = 80.0
 
+@onready var tree = $AnimationTree
+@onready var playback = tree.get("parameters/playback")
+
 
 func _physics_process(delta):
 	velocity.x = Input.get_axis('ui_left', 'ui_right') * move_speed
 	velocity.y = Input.get_axis('ui_up', 'ui_down') * move_speed
+	
+	if velocity != Vector2.ZERO:
+		playback.set('parameters/idle/blend_position', velocity)
+		playback.set('parameters/walk/blend_position', velocity)
+		
+		playback.travel('walk')
+	else:
+		playback.travel('idle')
 	
 	velocity.normalized()
 	
