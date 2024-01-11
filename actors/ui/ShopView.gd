8c13fa1ab2dfbdb3c1eb68aa0a8825efe5cb1d53
rockgem <63050183+rockgem@users.noreply.github.com>
2024-01-12 00:45:18 +0800

    changes in shop

--- actors/ui/ShopView.gd
+++ actors/ui/ShopView.gd
@@ -6,8 +6,19 @@ extends Panel
 var items_data = {}
 
 func _ready():
-	items_data = ManagerGame.get_data("res://reso/data/shop_data.json")
+	items_data = ManagerGame.get_data("res://reso/data/items_data.json")
+	var shop_items = ManagerGame.get_data("res://reso/data/shop_data.json")
 	
-	var items = items_data['items']
+	var items = shop_items['items']
 	for item in items:
-		pass
+		var data = items_data[item]
+		
+		var i = load("res://actors/ui/InvSlot.tscn").instantiate()
+		i.display(data)
+		i.tapped.connect(on_item_tapped)
+		
+		list.add_child(i)
+
+
+func on_item_tapped(data):
+	pass
